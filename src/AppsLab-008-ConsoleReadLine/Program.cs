d8170823347c2a70be5f846adad7d70eace6b0eb
turskyf24 <turskyf24@Z00T1-15>
2024-09-17 09:39:07 +0200

    dokoncenie cvicenia 008_ConsoleReadLine

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,8 +1,14 @@
 ﻿Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
-Console.WriteLine("Ahoj, " + meno);
+Console.WriteLine("Ahoj, " + meno + "\n");
 
 Console.WriteLine("Koľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
 Console.WriteLine("Máš " + vek + " rokov.");
+
+Console.WriteLine("ake je tvoje oblubene jedlo?");
+string? jedlo = Console.ReadLine();
+Console.WriteLine("mmmmm " + jedlo);
+
+Console.WriteLine("rikaju mi " + meno + " mam " + vek + " a mam rad " + jedlo);
