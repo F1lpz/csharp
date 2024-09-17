bf352859707362a1929ae7c8837b02f2fc2a6f3f
turskyf24 <turskyf24@Z00T1-15>
2024-09-17 11:10:43 +0200

    dokoncenie cvicenia 008_ConsoleReadLine

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Ako sa voláš?");
+﻿/*Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
 Console.WriteLine("Ahoj, " + meno + "\n");
 
@@ -11,4 +11,78 @@ Console.WriteLine("ake je tvoje oblubene jedlo?");
 string? jedlo = Console.ReadLine();
 Console.WriteLine("mmmmm " + jedlo);
 
-Console.WriteLine("rikaju mi " + meno + " mam " + vek + " a mam rad " + jedlo);
+Console.WriteLine("rikaju mi " + meno + " mam " + vek + " a mam rad " + jedlo);*/
+
+/*int num = 1_100;
+int num1 = 1_100;
+
+Console.WriteLine("");
+string vstup = Console.ReadLine() ?? "0";
+
+Console.WriteLine(1_100 + 1_100);*/
+
+
+/*int num = 20;
+int num1 = 21;
+
+Console.WriteLine(num == num1);
+Console.WriteLine(num != num1);
+Console.WriteLine(num > num1);
+Console.WriteLine(num < num1);
+Console.WriteLine(num >= num1);
+Console.WriteLine(num <= num1);*/
+
+
+
+
+/*var vysledok = (50 + 40);
+Console.WriteLine(vysledok);*/
+
+/*var menoU = "filip";
+var hesloU = "123";
+
+
+var zadaneM = Console.ReadLine() ?? "0";
+var zadaneH = Console.ReadLine() ?? "0";
+
+
+var porovnaniemena = menoU == zadaneM;
+var porovnaniehesla = hesloU == zadaneH;
+
+
+Console.WriteLine("Meno: " + porovnaniemena);
+Console.WriteLine("Heslo: " + porovnaniehesla);*/
+
+
+Console.WriteLine("zadajte meno a heslo:");
+
+var m = "filip";
+var h = "123";
+
+var zm = Console.ReadLine();
+var zh = Console.ReadLine();
+
+if (zh == h)
+
+{
+    Console.WriteLine("heslo sa zhoduje " + m);
+}
+else
+{
+    Console.WriteLine("heslo sa nezhoduje");
+}
+
+if (zm == m)
+{
+    Console.WriteLine("meno sa zhoduje");
+}
+       else
+{
+    Console.WriteLine("meno sa nezhoduje");
+}
+    
+
+
+
+
+
