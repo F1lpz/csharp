ec661a83be96d7e6f8eb378923cb863aa592ff6d
turskyf24 <turskyf24@Z00T1-15>
2024-09-26 09:09:05 +0200

    dokoncenie cvicenia podmienky

---
+++ test1/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            double num1 = 12;
+            double num2 = 11;
+            Console.WriteLine("zadaj operator");
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "*":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case "/":
+                    Console.WriteLine(num1 / num2);
+                    if
+                        {
+                        operation = 0; break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("zly operator");
+                    break;
+            }
+        }
+    }
+}
+
