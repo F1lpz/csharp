f5c5e4ece24ced4b285ce16f56d119e0c6dc9493
turskyf24 <turskyf24@Z00T1-15>
2024-10-01 10:48:46 +0200

    dokoncenie CYKLOV

---
+++ cvicenie_cykly/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listname = new List<string>();
+            {
+                listname.Add("FILIP");
+                listname.Add("OLINO");
+                listname.Add("BRANO");
+                listname.Add("MATO");
+                listname.Add("JOZO");
+                listname.Add("IGOR");
+
+                int counter = 0;
+                foreach (string name in listname)
+                {
+                Console.WriteLine(counter++ + name);
+                }
+            }
+        }
+    }
+}
--- cvicenie_list/Program.cs
+++ cvicenie_list/Program.cs
@@ -6,14 +6,32 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[5];
+           /* int[] numbers = new int[5];
             //10 15 20 25 45 
             numbers[0] = 10;
             numbers[1] = 15;
             numbers[2] = 20;
             numbers[3] = 25;
             numbers[4] = 40;
-            Console.WriteLine(numbers[2]);
+            Console.WriteLine(numbers[2]+ numbers[4]);*/
+
+
+
+            List<string> listname = new List<string>();
+
+            listname.Add("filip");
+            listname.Add("olino");
+            listname.Add("brano");
+            listname.Add("mato");
+            listname.Add("jozo");
+            listname.Add("igor");
+
+            Console.WriteLine(listname[0]);
+            Console.WriteLine(listname[1]);
+            Console.WriteLine(listname[2]);
+            Console.WriteLine(listname[3]);
+            Console.WriteLine(listname[4]);
+            Console.WriteLine(listname[5]);
         }
     }
 }
