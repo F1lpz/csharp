2acb304548e3fd4aed340945ad0771825b3fb6d8
turskyf24 <turskyf24@Z00T1-15>
2024-10-17 09:14:57 +0200

    opp hra

--- cvicenie OPP hra/Program.cs
+++ cvicenie OPP hra/Program.cs
@@ -1,26 +1,92 @@
-﻿namespace cvicenie_OPP_hra
+﻿using cvicenie_OPP_hra;
+using System.Runtime.CompilerServices;
+
+namespace Cvi4enie_OOP_Hra
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            player zemepan = new player("jozko" ,75,900,150);
-            player assasin = new player("nigi", 100, 1500, 320);
-
-            while (true)
+            player player = new player();
             {
-                zemepan.damagedplayer(assasin);
-                assasin.damagedplayer(zemepan);
-
-                int hp = 100;
-                if (hp < 0)
-                {
-                    break;
-                }
-                    
-                    
+                player.name = "B";
+                player.hp = 100;
+                player.mana = 8;
+                player.attackpower = 6;
+            }
+            player player2 = new player();
+            {
+                player2.name = "H";
+                player2.hp = 100;
+                player2.mana = 15;
+                player2.attackpower = 3;
             }
             
+            
+           
+
+            int HP = 100;
+            while (player.hp >= 0 && player2.hp > 0)
+            {
+                Thread.Sleep(700);
+                Console.WriteLine("player hp:" + player.hp);
+                Console.WriteLine("player2 hp:" + player2.hp);
+                player.damagedplayer(player2);
+                player2.damagedplayer(player);
+
+                if (player.hp <= 20)
+                {
+                    bool washealed = player.heal();
+
+                    if (washealed)
+                    {
+                        Console.WriteLine("player bol uzdraveny");
+                    }
+                    else
+                    {
+                        Console.WriteLine("player nebol uzdraveny");
+                    }
+
+                }
+                if (player2.hp <= 20)
+                {
+                    bool washealed = player.heal();
+
+                    if (washealed)
+                    {
+                        Console.WriteLine("player2 bol uzdraveny");
+                    }
+                    else
+                    {
+                        Console.WriteLine("player2 nebol uzdraveny");
+                    }
+
+                }
+
+
+                if (player.hp <= 0)
+                {
+                    Console.WriteLine("Player1 vyhral");
+                }
+                else if (player2.hp <= 0)
+                {
+                    Console.WriteLine("Player2 vyhral");
+                }
+
+                player.refilmana(10);
+                Random random = new Random();
+                int randomnumber = random.Next(0, 100);
+                if (randomnumber <= 5) ;
+                {
+                    player.refilmana(10);
+                    Console.WriteLine("mana bola doplnena.");
+
+                }
+            }
+
+
+
+
+            }
         }
     }
-}
--- cvicenie OPP hra/player.cs
+++ cvicenie OPP hra/player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,10 @@ namespace cvicenie_OPP_hra
         public int mana { get; set; }
         public int attackpower { get; set; }
 
+        public int critchange { get; set; }
+        public player()
+        {
+        }
 
         public player(string name, int hp, int mana, int attackpower)
         {
@@ -24,14 +29,61 @@ namespace cvicenie_OPP_hra
         }
         public void damagedplayer(player player)
         {
+
+            int damagemultiplier = 1;
+
+            Random random = new Random();
+            int randomnumber = random.Next(0, 100);
+            if (randomnumber <= this.critchange)
+            {
+                damagemultiplier = 2;
+                Console.WriteLine(this.name + "dal criticky zasah");
+            }
+
+
+
+
+
+
             int hpofenemy = player.hp;
             int attackofcurrentplayer = this.attackpower;
             int hpofenemyafterfight = hpofenemy - attackofcurrentplayer;
             player.hp = hpofenemyafterfight;
         }
 
+        public bool heal()
+        {
 
+            if (this.mana > 0)
+            {
+                int hpofplayer = this.hp;
+                int manaofplayer = this.mana;
+                int hpofplayerafterheal = hpofplayer + manaofplayer;
+                this.mana = 0;
+                this.hp = hpofplayerafterheal;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             
+            
+            
+
+
+        }
+        
+        public void refilmana(int newmana)
+        {
+            this.mana = newmana;
+            Random random = new Random();
+            int randomnumberv= random.Next(0, 100);
+            if (randomnumberv <= this.mana)
+            {
+                Console.WriteLine(this.mana + "many bolo doplnene");
+            }
         }
     }
+}
 
